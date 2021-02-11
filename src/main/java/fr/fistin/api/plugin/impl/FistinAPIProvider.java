d8fe099764c68c155db92d38ebee2848c1830167
FlowArg <coutureflorence3@gmail.com>
2021-02-11 16:04:56 +0100

    Fixed reflection problem on plugin loading

--- src/main/java/fr/fistin/api/plugin/impl/FistinAPIProvider.java
+++ src/main/java/fr/fistin/api/plugin/impl/FistinAPIProvider.java
@@ -27,104 +27,104 @@ import java.io.IOException;
 import java.util.logging.Level;
 
 @Internal
-final class FistinAPIProvider extends JavaPlugin implements IFistinAPIProvider
+public final class FistinAPIProvider extends JavaPlugin implements IFistinAPIProvider
 {
-	private FireworkFactory fireworkFactory;
-	private PacketManager packetManager;
-	private IFistinEventExecutor eventExecutor;
-	private IFistinEventBus eventBus;
-	private DatabaseManager databaseManager;
+    private FireworkFactory fireworkFactory;
+    private PacketManager packetManager;
+    private IFistinEventExecutor eventExecutor;
+    private IFistinEventBus eventBus;
+    private DatabaseManager databaseManager;
 
-	@Override
-	public void onEnable()
-	{
-		this.getLogger().info("Entering initialization phase...");
+    @Override
+    public void onEnable()
+    {
+        this.getLogger().info("Entering initialization phase...");
 
-		this.preInit();
-		this.init();
-		this.postInit();
-	}
+        this.preInit();
+        this.init();
+        this.postInit();
+    }
 
-	private void preInit()
-	{
-		this.saveDefaultConfig();
-		this.reloadConfig();
-		PluginProviders.setProvider(IFistinAPIProvider.class, this);
-		PluginProviders.setProvider(ILevelingProvider.class, new LevelingProvider());
-		ConfigurationProviders.setConfig(DatabaseConfiguration.class, new DatabaseConfiguration());
-	}
+    private void preInit()
+    {
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        PluginProviders.setProvider(IFistinAPIProvider.class, this);
+        PluginProviders.setProvider(ILevelingProvider.class, new LevelingProvider());
+        ConfigurationProviders.setConfig(DatabaseConfiguration.class, new DatabaseConfiguration());
+    }
 
-	private void init()
-	{
-		this.eventExecutor = new InternalFistinEventExecutor();
-		this.eventBus = new InternalEventBus();
-		this.databaseManager = new DatabaseManager();
-		this.fireworkFactory = new FireworkFactory();
-		this.packetManager = new PacketManager();
-	}
+    private void init()
+    {
+        this.eventExecutor = new InternalFistinEventExecutor();
+        this.eventBus = new InternalEventBus();
+        this.databaseManager = new DatabaseManager();
+        this.fireworkFactory = new FireworkFactory();
+        this.packetManager = new PacketManager();
+    }
 
-	private void postInit()
-	{
-		this.eventExecutor.getRegisterer().addEventBus(this.eventBus);
-		this.fireworkFactory.registerFirework(new PluginLocation(NAMESPACE, "firstSetup", true), bd -> bd.flicker(true).trail(true).with(FireworkEffect.Type.BURST).withColor(Color.PURPLE, Color.BLUE, Color.YELLOW).withFade(Color.ORANGE).build());
+    private void postInit()
+    {
+        this.eventExecutor.getRegisterer().addEventBus(this.eventBus);
+        this.fireworkFactory.registerFirework(new PluginLocation(NAMESPACE, "firstSetup", true), bd -> bd.flicker(true).trail(true).with(FireworkEffect.Type.BURST).withColor(Color.PURPLE, Color.BLUE, Color.YELLOW).withFade(Color.ORANGE).build());
 
-		this.packetManager.registerPacket(FReturnToBungeePacket.class, packet -> {
-			try
-			{
-				final ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-				final DataOutputStream out = new DataOutputStream(byteArray);
+        this.packetManager.registerPacket(FReturnToBungeePacket.class, packet -> {
+            try
+            {
+                final ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+                final DataOutputStream out = new DataOutputStream(byteArray);
 
-				out.writeUTF("Connect");
-				out.writeUTF(packet.getServerName());
-				packet.getToSend().sendPluginMessage(packet.getPlugin(), packet.getBungeeCordChannel(), byteArray.toByteArray());
-			}
-			catch (IOException e)
-			{
-				this.getLogger().log(Level.SEVERE, e.getMessage(), e);
-			}
-		});
+                out.writeUTF("Connect");
+                out.writeUTF(packet.getServerName());
+                packet.getToSend().sendPluginMessage(packet.getPlugin(), packet.getBungeeCordChannel(), byteArray.toByteArray());
+            }
+            catch (IOException e)
+            {
+                this.getLogger().log(Level.SEVERE, e.getMessage(), e);
+            }
+        });
 
-		this.getServer().getPluginManager().registerEvents(new SetupListener(), this);
-	}
-	
-	@Override
-	public void onDisable()
-	{
-		this.databaseManager.close();
-		this.packetManager.clear();
-		this.fireworkFactory.clear();
-		PluginProviders.clear();
-		ConfigurationProviders.clear();
-		this.eventExecutor.clear();
-	}
+        this.getServer().getPluginManager().registerEvents(new SetupListener(), this);
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        this.databaseManager.close();
+        this.packetManager.clear();
+        this.fireworkFactory.clear();
+        PluginProviders.clear();
+        ConfigurationProviders.clear();
+        this.eventExecutor.clear();
+    }
 
-	@Override
-	public IFistinEventBus getEventBus()
-	{
-		return this.eventBus;
-	}
+    @Override
+    public IFistinEventBus getEventBus()
+    {
+        return this.eventBus;
+    }
 
-	@Override
-	public IFistinEventBusRegisterer getEventRegisterer()
-	{
-		return this.eventExecutor.getRegisterer();
-	}
+    @Override
+    public IFistinEventBusRegisterer getEventRegisterer()
+    {
+        return this.eventExecutor.getRegisterer();
+    }
 
-	@Override
-	public FireworkFactory getFireworkFactory()
-	{
-		return this.fireworkFactory;
-	}
+    @Override
+    public FireworkFactory getFireworkFactory()
+    {
+        return this.fireworkFactory;
+    }
 
-	@Override
-	public PacketManager getPacketManager()
-	{
-		return this.packetManager;
-	}
+    @Override
+    public PacketManager getPacketManager()
+    {
+        return this.packetManager;
+    }
 
-	@Override
-	public DatabaseManager getDatabaseManager()
-	{
-		return this.databaseManager;
-	}
+    @Override
+    public DatabaseManager getDatabaseManager()
+    {
+        return this.databaseManager;
+    }
 }
